5d6d04f3040a1b3eb49855772c95e7229e398ff4
masaki.yamakawa <masaki.yamakawa@ulsystems.co.jp>
2021-09-23 12:07:26 +0900

    Add function of change map style

--- webapp/frontend/src/pages/estate/nazottemb/index.tsx
+++ webapp/frontend/src/pages/estate/nazottemb/index.tsx
@@ -5,21 +5,31 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWFzYWtpLXlhbWFrYXdhIiwiYSI6ImNrcWE3NGRsdDA3Z
 
 
 const NazotteMapBoxPage: React.FC = () => {
+  const [mapInstance, setMapInstance] = useState<mapboxgl.Map>();
   const mapContainer = useRef<HTMLDivElement | null>(null);
 
   const [lng, setLng] = useState(142.0);
   const [lat, setLat] = useState(40.0);
   const [zoom, setZoom] = useState(4);
 
+  const [mapStyle, setMapStyle] = useState('satellite');
+
+  const changeStyle = (e: any) => {
+    console.log("changeStyle:style=" + e.target.id);
+    setMapStyle(e.target.value);
+    mapInstance.setStyle('mapbox://styles/mapbox/' + e.target.id);
+  };
+
   useEffect(() => {
     if (!mapContainer.current) return;
 
     const map = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
+      style: 'mapbox://styles/mapbox/satellite-v9',
       center: [lng, lat],
       zoom: zoom
     });
+    setMapInstance(map);
 
     map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
@@ -38,6 +48,23 @@ const NazotteMapBoxPage: React.FC = () => {
         <div>
           Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
         </div>
+      </div>
+      <div className="mapstyleStyle">
+        <label>
+          <input id="satellite-v9" type="radio" name="rtoggle" value="satellite" onChange={changeStyle} checked={mapStyle === 'satellite'} />satellite
+        </label>
+        <label>
+          <input id="light-v10" type="radio" name="rtoggle" value="light" onChange={changeStyle} checked={mapStyle === 'light'} />light
+        </label>
+        <label>
+          <input id="dark-v10" type="radio" name="rtoggle" value="dark" onChange={changeStyle} checked={mapStyle === 'dark'} />dark
+        </label>
+        <label>
+          <input id="streets-v11" type="radio" name="rtoggle" value="streets" onChange={changeStyle} checked={mapStyle === 'streets'} />streets
+        </label>
+        <label>
+          <input id="outdoors-v11" type="radio" name="rtoggle" value="outdoors" onChange={changeStyle} checked={mapStyle === 'outdoors'} />outdoors
+        </label>
       </div>
       <div className='map-container' ref={mapContainer} />
     </div>
